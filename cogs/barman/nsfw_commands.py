c2cfccba3a056fff8581160e9e09202fa0a55375
EthannSchneider <ethann.schneider@swisssign.com>
2023-08-11 15:29:06 +0200

    Renammed sex commands to nsfw commands

---
+++ cogs/barman/nsfw_commands.py
@@ -0,0 +1,34 @@
+from libs.log import Log
+import traceback
+import discord
+import os
+
+from libs.config import Config
+from libs.log import LogType
+from libs.utils import Utils
+
+class NsfwCommands(discord.Cog):
+    def __init__(self, bot: discord.bot.Bot) -> None:
+        self.__bot = bot
+        self.__config = Config()
+
+    @discord.slash_command(description="Get some NSFW content in a NSFW channel")
+    @discord.option("choose", description="", choices=["porn", "hentai", "jinx", "002", "overwatch"])
+    async def porn(self, ctx: discord.commands.context.ApplicationContext, choose : str):
+        Log(ctx.author.name + " is launching sex commands with " + choose, LogType.COMMAND)
+        try:
+            if not ctx.channel.nsfw:
+                await ctx.respond("This command can only be used in a NSFW channel")
+                return 
+            path = self.__config.value["sex_commands"][choose]
+            if path != "" and os.path.isdir(path):
+                await ctx.respond(file=discord.File(Utils().random_file(path)))
+            else:
+                await ctx.respond("No porn folder found!")
+        except:
+            await ctx.respond("An error has occurred, cannot get your porn now please try again later.")
+            Log(traceback.format_exc(), LogType.ERROR)
+
+def setup(bot: discord.bot.Bot):
+    if Config().value["nsfw_commands"]["enable"]:
+        bot.add_cog(NsfwCommands(bot))
