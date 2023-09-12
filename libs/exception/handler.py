8d76ff1f3167418073b3a6bba642bf3eb0e95996
EthannSchneider <ethann.schneider@swisssign.com>
2023-09-12 20:13:41 +0200

    WIP error reponse handler

--- libs/exception/handler.py
+++ libs/exception/handler.py
@@ -1,8 +1,20 @@
 from libs.config import Config
+from libs.exception.bot_exception import BotException
 
 class Handler:
     def __init__(self) -> None:
         self.__config = Config()
         
     def response_handler(self, exception: Exception) -> str:
-        pass 
+        if isinstance(exception, BotException):
+            match(str(exception)):
+                case "NotConnectedToVoiceChannelException":
+                    pass
+                case "NotPlayingMusicException":
+                    pass
+                case "NothingLeftInPreviousQueueException":
+                    return self.__config.value["exception_response"]["not_connected_to_voice_channel"]
+                case _:
+                    return self.__config.value["exception_response"]["default"]
+        else:
+            return self.__config.value["exception_response"]["default"]
