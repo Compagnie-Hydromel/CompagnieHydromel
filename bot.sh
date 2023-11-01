48e9bd2fcc182dbfeb6818be6b444051d9b4802e
EthannSchneider <ethann.schneider@swisssign.com>
2023-11-01 15:28:39 +0100

    Updated bot.sh and changed vscode env to launch debug

--- bot.sh
+++ bot.sh
@@ -1,13 +1,17 @@
+cd $(dirname "$0")
+
 source env/bin/activate
 
-python3 bot.py &
+python3 bot.py barman &
 barman_pid=$!
 
-python3 bot.py &
+python3 bot.py menestrel &
 menestrel_pid=$!
 
-python3 bot.py &
+python3 bot.py archiveuse &
 archiveuse_pid=$!
+
+cd ..
 
 wait $barman_pid
 wait $menestrel_pid
