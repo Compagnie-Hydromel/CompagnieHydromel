a1a5d1d034e4f5c624bed9489ee1062ae3d220d3
EthannSchneider <ethann.schneider@swisssign.com>
2023-11-13 11:21:53 +0100

    Added unit test to check you cannot remove the default profile layout

--- tests/test_profile_layouts.py
+++ tests/test_profile_layouts.py
@@ -4,6 +4,7 @@ from libs.databases.dto.layout import Layout
 from libs.databases.model.profile_layout.profile_layout import ProfileLayout
 
 from libs.databases.model.profile_layout.profile_layouts import ProfileLayouts
+from libs.exception.profile_layout.cannot_remove_default_profile_layout import CannotRemoveDefaultProfileLayout
 
 class TestProfileLayout(unittest.TestCase):
     def __init__(self, methodName: str = "runTest") -> None:
@@ -32,6 +33,10 @@ class TestProfileLayout(unittest.TestCase):
         self.__profile_layouts.remove(ProfileLayout(profile_layout_new_name))
         
         self.assertFalse(self.__contains_profile_layout(profile_layout_new_name))
+        
+    def test_cannot_remove_default_profile_layout(self):
+        with self.assertRaises(CannotRemoveDefaultProfileLayout):
+            self.__profile_layouts.remove(ProfileLayout.get_default())
         
     def __contains_profile_layout(self, name: str) -> bool:
         for profile_layout in self.__profile_layouts.get_all_profile_layouts:
