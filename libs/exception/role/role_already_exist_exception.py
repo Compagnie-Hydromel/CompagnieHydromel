93c1a6a9c24365968f0d7919e217bd8c130ec678
Ethann Schneider <ethann@shkermit.ch>
2024-04-29 12:10:52 +0200

    #26 Added role class + exception

---
+++ libs/exception/role/role_already_exist_exception.py
@@ -0,0 +1,6 @@
+from libs.exception.bot_exception import BotException
+
+class RoleAlreadyExistException(BotException):
+    """This class is designed to manage the exception when the role already exist.
+    """
+    pass
