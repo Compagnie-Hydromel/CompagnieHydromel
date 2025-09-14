cca2fd87587fd36428a18160ef9752e4f7eb5e84
Ethann <ethann@shkermit.ch>
2025-09-14 21:57:28 +0200

    feat: Add proxy configuration for API requests in vite.config.ts

--- http/frontend/vite.config.ts
+++ http/frontend/vite.config.ts
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': 'http://127.0.0.1:8000',
+    },
+  },
 })
